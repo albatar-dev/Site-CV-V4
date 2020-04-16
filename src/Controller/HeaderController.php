9ea0e64b18244c316e2bcc785034012a11863a06
albatar-dev <jocelyn.arzur@free.fr>
2020-04-16 23:08:01 +0200

    achevement de la page projets Ajout des projets et remise en forme de la navbar

---
+++ src/Controller/HeaderController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProjetsRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HeaderController extends AbstractController
+{
+    public function projets(ProjetsRepository $repo)
+    {
+        $projets = $repo->findAll();
+        return $this->render('partials/_header.html.twig', compact('projets'));
+    }
+}
