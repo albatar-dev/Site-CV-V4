95f1dc587214d2a70d3d0dae6711b27f68577c2d
albatar-dev <jocelyn.arzur@free.fr>
2020-04-17 19:32:45 +0200

    ajout de la creation/suppression de texteAccueil pour l'admin

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\TexteAccueil;
 use App\Form\TexteAccueilFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\TexteAccueilRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,6 +23,34 @@ class AccueilController extends AbstractController
         $user = $this->getUser();
         return $this->render('accueil/index.html.twig', compact('donnees', 'user'));
     }
+
+    /**
+     * @route("/accueil/create",name="accueil.create", methods={"GET", "POST"})
+     */
+     function create(Request $request, EntityManagerInterface $em)
+     {
+         $texteAccueil = new TexteAccueil();
+ 
+         $form = $this->createForm(TexteAccueilFormType::class, $texteAccueil);
+ 
+ 
+         $form->handleRequest($request);
+ 
+         if($form->isSubmitted() && $form->isValid()) {
+             $em->persist($texteAccueil);
+             $em->flush();
+ 
+             $this->addFlash('success', 'Billet créé');
+ 
+             return $this->redirectToRoute('accueil');
+         }
+ 
+ 
+         return $this->render('accueil/partials/_form.html.twig', [
+             'event'=>$texteAccueil,
+             'form'=>$form->createView()
+         ]);
+     }
 
     /**
      * @Route("/accueil/{id}/edit", name="accueil.edit", methods={"GET", "PATCH"})
@@ -43,4 +72,19 @@ class AccueilController extends AbstractController
 
         return $this->render('accueil/edit.html.twig', ['form'=>$form->createView()]);
     }
+
+
+    /**
+     * @Route("/accueil/{id}/delete", name="accueil.delete", methods={"DELETE"})
+     */
+     public function delete(TexteAccueil $texteAccueil, Request $request, EntityManagerInterface $em){
+        if ($this->isCsrfTokenValid('delete', $request->request->get('_token'))) {
+            $em->remove($texteAccueil);
+            $em->flush();
+
+            $this->addFlash('danger', 'Billet supprimé');
+        }
+
+        return $this->redirectToRoute('accueil');
+     }
 }
